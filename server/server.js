798e76c556ab94720fac8ad30482954653f66853
Gyulo94 <qkrcjfrb112@gmail.com>
2024-02-02 13:19:04 +0900

    영어 문의 요청사항 조건 수정2

--- server/server.js
+++ server/server.js
@@ -7,7 +7,19 @@ const { PORT } = require("./common");
 const mongo = require("./mongo");
 require("dotenv").config();
 
-app.use(cors("http://petsmeal.wevibe.co.kr/"));
+const allowed = ["http://petsmeal.wevibe.co.kr/", "http://localhost:3000/"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowed.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS 가 허용되지 않았습니다."));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
